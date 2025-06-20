ae9b673e42c4eaf15cbe06493dea86e2db82166b
KremasLagos <stathxmkourtis@gmail.com>
2025-06-20 15:11:40 +0300

    Added operator functionality User can add subtruct multiply etc Display is working Bugs expected

--- code.js
+++ code.js
@@ -1,34 +1,77 @@
-function add(a,b){
-return a + b
+let firstMember = 0;
+let secondMember = 0;
+let displayMember = "";
+let currentOperator = "";
+// Numbers loop
+let numbers = document.getElementsByClassName("number");
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", () =>
+    addNumberToString(displayMember, numbers[i].textContent)
+  );
 }
-function multiply(a,b){
-return a * b
+
+// Equal
+document.getElementsByClassName("equal")[0].addEventListener("click", () => {
+  displayMember = operate(firstMember, currentOperator, displayMember);
+  display(displayMember);
+  // alert("works")
+});
+
+// Operators loop
+let operators = document.getElementsByClassName("operator");
+for (let i = 0; i < operators.length; i++) {
+  operators[i].addEventListener("click", () => {
+    currentOperator = String(operators[i].textContent);
+    fromFirstToSecond();
+  });
 }
-function subtract(a,b){
-return a -b
+
+document.getElementById("clear").addEventListener("click", () => {
+  firstMember = 0;
+  secondMember = 0;
+  displayMember = "";
+  currentOperator = "";
+  display(displayMember)
+});
+
+function add(a, b) {
+  return a + b;
 }
-function divide(a,b){
-return a / b
+function multiply(a, b) {
+  return a * b;
 }
-function operate(numA, operator, numB){
-if (operator === "+")
-{
-    add(numA,numB)
+function subtract(a, b) {
+  return a - b;
 }
-else if (operator === "*")
-{
-    multiply(numA,numB)
+function divide(a, b) {
+  return a / b;
 }
-else if (operator === "-")
-{
-    subtract(numA,numB)
+function operate(numA, operator, numB) {
+  if (operator === "+") {
+    return add(numA, numB);
+  } else if (operator === "X") {
+    return multiply(numA, numB);
+  } else if (operator === "-") {
+    return subtract(numA, numB);
+  } else if (operator === "/") {
+    return divide(numA, numB);
+  } else {
+    return alert("operator error");
+  }
 }
-else if(operator === "/")
-{
-    divide(numA,numB)
+
+function display(num) {
+  let screen = document.querySelector(".screen");
+  screen.textContent = num;
 }
-else
-{
-    alert("operator error")
+
+function addNumberToString(a = "", b = "") {
+  displayMember = String(displayMember) + String(b);
+  display(displayMember);
+  return;
 }
+
+function fromFirstToSecond() {
+  firstMember = displayMember;
+  displayMember = "";
 }
